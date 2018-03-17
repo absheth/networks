8b37ff2fbbcfab9b4d83e2dfbccba15f71fa8049
Akash B. Sheth <akashbsheth@gmail.com>
2018-03-17 04:43:56 -0400

    packet struct declaration

--- reliable_udp/code/client_r_udp.cpp
+++ reliable_udp/code/client_r_udp.cpp
@@ -18,7 +18,23 @@ extern void perror(const char *__s);
 #define SUCCESS 0
 #define FAILURE -1
 #define MAXBUFFERSIZE 99999
+// -----------------------------------------------------------------------
 
+#define DATA_SIZE 1560
+struct message {
+    char data[DATA_SIZE];
+};
+
+struct pkt {
+    int seq_num;
+    int ack_num;
+    char ack_flg;  // THINK
+    char load[DATA_SIZE];
+};
+
+void make_packet_data(char *p_data_str, pkt p_packet);
+
+// -----------------------------------------------------------------------
 int main(int argc, char const *argv[]) {
     std::cout << '\n';
     std::cout << "---------------------------- START ----------------------------" << '\n';
@@ -32,35 +48,26 @@ int main(int argc, char const *argv[]) {
     char clientIP[INET6_ADDRSTRLEN];
     // -----------------------------------------------------------------------
     std::cout << "argc --> " << argc << '\n';
-
-    std::string server_host = argv[1];
-    int server_port = atoi(argv[2]);
+    // std::string server_host = argv[1];
+    // int server_port = atoi(argv[2]);
+    std::string server_host = "localhost";
+    int server_port = 9158;
+    std::string http_rqst = "GET /1mb.txt HTTP/1.1";
+    /*
     total_files = argc - 3;
     std::string files[2];
     int i;
     for (i = 3; i < argc; i++) {
         files[i - 3] = argv[i];
     }
-    // std::cout << "server_host --> " << server_host<<'\n';
-    // std::cout << "server_port --> " << server_port <<'\n';
-    // std::cout << "total_files --> " << total_files <<'\n';
-    // for(int i = 0; i < total_files; i++) {
-    //         std::cout << "file " << i << " --> " << files[i]<< '\n';
-    // }
-    // std::stringstream ss;
-    // ss << "GET /";
-    // ss << files[0];
-    // ss << " HTTP/1.1";
-    // std::string temp;
-    // temp = ss.str();
-    // std::cout << "request --> " << temp.c_str() << " and the length is " << temp.length() <<'\n';
-    //
+    */
+    // -----------------------------------------------------------------------
     socketFD = socket(AF_INET, SOCK_DGRAM, 0);
+    std::cout << "SOCKET --> " << socketFD << std::endl;
     if (socketFD < 0) {
         std::perror("Error while opening the socket");
         return EXIT_FAILURE;
     }
-
     server = gethostbyname(server_host.c_str());
     if (server == NULL) {
         std::cerr << "no host --> " << server_host.c_str() << '\n';
@@ -73,16 +80,33 @@ int main(int argc, char const *argv[]) {
     serveraddr.sin_port = htons(server_port);
     std::cout << "Server ==> " << server_host << '\n';
     std::cout << "Port ==> " << server_port << '\n';
-
     serverlen = sizeof(serveraddr);
+    // -----------------------------------------------------------------------
+    // TRIAL 
+    pkt rqst_packet;
+    rqst_packet.ack_num = -1;   // THINK
+    rqst_packet.ack_flg = 'x';  // or a
+    rqst_packet.seq_num = 0;    // THINK
+
     std::stringstream request;
-    request << "GET /";
-    request << files[0];
-    request << " HTTP/1.1\n";
-    std::string temp_str;
-    temp_str = request.str();
-    std::cout << "REQUEST --> " << request.str().c_str() << '\n';
-    // Get starting timepoint
+    request << rqst_packet.ack_num;  // ERROR. NEEDS TO BE FIXED. CANNOT BE DONE THIS WAY.
+    request << ",";
+    request << rqst_packet.ack_flg;  // ERROR. NEEDS TO BE FIXED. CANNOT BE DONE THIS WAY.
+    request << ",";
+    request << rqst_packet.seq_num;  // ERROR. NEEDS TO BE FIXED. CANNOT BE DONE THIS WAY.
+    request << ",";
+    request << http_rqst;
+    std::cout << "REQUEST --> " << request.str().c_str() << " | length = " << request.str().length()
+              << std::endl;
+    std::cout << "" << std::endl;
+    strcpy(rqst_packet.load, request.str().c_str());
+    std::cout << "rqst_packet.ack_num --> " << rqst_packet.ack_num << std::endl;
+    std::cout << "rqst_packet.ack_flg --> " << rqst_packet.ack_flg << std::endl;
+    std::cout << "rqst_packet.seq_num --> " << rqst_packet.seq_num << std::endl;
+    std::cout << "rqst_packet.load --> " << rqst_packet.load << std::endl;
+
+    return 0;
+
     auto start = std::chrono::high_resolution_clock::now();
     if (sendto(socketFD, request.str().c_str(), request.str().length(), 0,
                (struct sockaddr *)&serveraddr, serverlen) < 0) {
@@ -98,17 +122,10 @@ int main(int argc, char const *argv[]) {
             exit(-1);
         }
         std::string x(buffer);
-        // std::cout << "received --> " << received << '\n';
-        // if (received == 0) {
-        //        break;
-        //}
         if (x == "**over**") {
             break;
         }
         size += received;
-
-        // std::cout << "x --> " << x << '\n';
-
         std::cout << buffer << '\n';
         memset(buffer, 0, sizeof buffer);
     }
@@ -126,3 +143,10 @@ int main(int argc, char const *argv[]) {
     return 0;
 }
 // -----------------------------------------------------------------------
+void make_packet_data(char *p_data_str, pkt p_packet) {
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+}
--- reliable_udp/code/trial.cpp
+++ reliable_udp/code/trial.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <sstream>
+struct pkt {
+    int seqnum;
+    int acknum;
+    int checksum;
+    char payload[26];
+};
 
 int main(int argc, char **argv) {
     std::cout << std::endl;
-    std::cout << "Number of arguments --> " << argc << std::endl;
-    std::cout << "Argument --> " << argv[0] << std::endl;
-
     std::cout << "size of char --> " << sizeof(char) << std::endl;
     std::cout << "size of int --> " << sizeof(int) << std::endl;
-    std::stringstream ss;
-    ss << "Akash Sheth";
-    std::cout << sizeof(ss)<< std::endl;
-    ss << 1;
-    std::cout << sizeof(ss)<< std::endl;
-    ss <<"1";
-    std::cout << sizeof(ss)<< std::endl;
+    // std::stringstream ss;
+    // ss << "Akash Sheth";
+    // std::cout << ss.str() << " and length is " << ss.str().length() << std::endl;
+    struct pkt packet;
+    packet.seqnum = 1;
+    packet.acknum = 1;
+    std::string x = "Akash Sheth is a good boy";
+    std::cout << "x.length --> "<< x.length() << std::endl;
     
-    std::cout<< ss.str() << std::endl;
-     
-    
-    
+    strncpy(packet.payload, x.c_str(), sizeof(packet.payload));
+    std::cout << "Packet.seqnum --> " << packet.seqnum << std::endl;
+    std::cout << "Packet.payload --> " << packet.payload << " | Length --> "
+              << strlen(packet.payload) << " | size: " << sizeof(packet.payload) << std::endl;
+    std::cout << "Packet.acknum --> " << packet.acknum << std::endl;
+
     return 0;
 }
